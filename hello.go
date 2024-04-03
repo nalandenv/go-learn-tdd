6f0c1955db39c63d28a90849bca9dfc41d61beaf
Nikhil Nalande <nalandenv@Nikhils-MacBook-Air.local>
2024-04-03 23:57:47 +0530

    added switch case with more functionality and test cases

--- hello.go
+++ hello.go
@@ -1,10 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func Hello(name string) string {
-	return "Hello, " + name
+const ( //grouped together all the constants
+	spanish               = "Spanish"
+	french                = "French"
+	marathi               = "Marathi"
+	englishGreetingPrefix = "Hello, "
+	spanishGreetingPrefix = "Hola, "
+	frenchGreetingPrefix  = "Bonjour, "
+	marathiGreetingPrefix = "Namaskar, "
+)
+
+func Hello(name string, language string) string { //public function as name starts with capital letter
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) { // function name starting with small letter are private
+	switch language {
+	case marathi:
+		prefix = marathiGreetingPrefix
+	case french:
+		prefix = frenchGreetingPrefix
+	case spanish:
+		prefix = spanishGreetingPrefix
+	default:
+		prefix = englishGreetingPrefix
+	}
+	return // (prefix string) is a named return so whatever is store in it will be returned just like return prefix
 }
 func main() {
-	fmt.Println(Hello("Nikhil"))
+	fmt.Println(Hello("Nikhil", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -3,9 +3,35 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Nikhil")
-	want := "Hello, Nikhil"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("John", "")
+		want := "Hello, John"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Mister", "Spanish")
+		want := "Hola, Mister"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Ervin", "French")
+		want := "Bonjour, Ervin"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("Greeting in Marathi", func(t *testing.T) {
+		got := Hello("Nikhil", "Marathi")
+		want := "Namaskar, Nikhil"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper() //this is needed to tell the test suite that this method is a helper. By doing this when it fails the line number reported will be in our function call rather than inside our test helper.
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
